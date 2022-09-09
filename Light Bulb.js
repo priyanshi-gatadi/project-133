a75907f90a83dab85c26bf18564cff889d77c8e7
priyanshi-gatadi <96427688+priyanshi-gatadi@users.noreply.github.com>
2022-09-09 16:29:36 +0530

    Add files via upload

---
+++ Light Bulb.js
@@ -0,0 +1,44 @@
+Status = "";
+LightBulb_image = "";
+objects = [];
+
+function preload(){
+    LightBulb_image = loadImage("https://cdn11.bigcommerce.com/s-dwlxj/images/stencil/2560w/products/3146/5285/60watt-edison-bulb__02843.1508283240.jpg?c=2");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(LightBulb_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(Light_Bulb_image,0,0,640,350);
+    if(Status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            fill("#fc0303");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x, objects[i].y);
+            noFill();
+            stroke("#fc0303");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
---
+++ fan.js
@@ -0,0 +1,45 @@
+Status = "";
+fan_image = "";
+objects = [];
+
+
+function preload(){
+    fan_image = loadImage("https://i5.walmartimages.com/asr/dd6ff603-9e3a-40c5-a54b-52bf6f424b33_1.13e5bac028cefa9ca2c98d703ce4f65b.jpeg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(fan_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(fan_image,0,0,640,350);
+    if(Status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            fill("#fc0303");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x - 14, objects[i].y - 175);
+            noFill();
+            stroke("#fc0303");
+            rect(objects[i].x - 14, objects[i].y - 175, objects[i].width - 2326, objects[i].height - 2850);
+        }
+    }
+}
